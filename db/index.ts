f7ffcc4a49dcb01a92c32b24a1cd26ebebd1b12a
20yuteo <20yuteo@gmail.com>
2024-03-20 21:08:35 +0900

    envファイルで管理するように変更

--- db/index.ts
+++ db/index.ts
@@ -4,9 +4,9 @@ import { Client } from "pg";
 const client = new Client({
   host: "127.0.0.1",
   port: 5432,
-  user: "web-tool",
-  password: "webtoolpassword",
-  database: "web-tool",
+  user: process.env.POSTGRES_USER || "web-tool",
+  password: process.env.POSTGRES_PASSWORD || "webtoolpassword",
+  database: process.env.POSTGRES_DB || "web-tool",
 });
 
 (async () => {

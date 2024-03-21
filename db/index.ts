a3c4e8090d92bde273108527fafe8e837fcf6474
20yuteo <20yuteo@gmail.com>
2024-03-21 22:56:51 +0900

    db接続調整

--- db/index.ts
+++ db/index.ts
@@ -1,16 +1,30 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { neon } from "@neondatabase/serverless";
+import { NodePgDatabase, drizzle } from "drizzle-orm/node-postgres";
+import {
+  NeonHttpDatabase,
+  drizzle as neonDrizzle,
+} from "drizzle-orm/neon-http";
 import { Client } from "pg";
 
-const client = new Client({
-  host: "127.0.0.1",
-  port: 5432,
-  user: process.env.POSTGRES_USER || "web-tool",
-  password: process.env.POSTGRES_PASSWORD || "webtoolpassword",
-  database: process.env.POSTGRES_DB || "web-tool",
-});
+let db:
+  | NodePgDatabase<Record<string, never>>
+  | NeonHttpDatabase<Record<string, never>>;
 
-(async () => {
-  client.connect();
-})();
+if (process.env.IS_OFFLINE) {
+  const client = new Client({
+    host: "127.0.0.1",
+    port: 5432,
+    user: process.env.POSTGRES_USER || "web-tool",
+    password: process.env.POSTGRES_PASSWORD || "webtoolpassword",
+    database: process.env.POSTGRES_DB || "web-tool",
+  });
 
-export const db = drizzle(client);
+  (async () => {
+    await client.connect();
+  })();
+  db = drizzle(client);
+} else {
+  db = neonDrizzle(neon(process.env.DRIZZLE_DATABASE_URL!));
+}
+
+export { db };
